4b43aedd051c76d9a8a9078d561451df6c756f9a
Quentin Verdier <quent1.verdier@gmail.com>
2023-12-07 10:56:41 +0100

    Add models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name=  models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+    catchphrase = models.CharField(max_length=300)
+
+    def str(self):
+        return  self.name
